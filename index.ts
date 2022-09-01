2f25c11f5ad8412a2f6a77e1085b31151519cdd7
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-09-01 22:33:27 +0200

    Post method finished

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ app.use(cors())
 app.use(express.json())
 const port = 5000
 
+//Home Page
+
 app.get('/', (req, res) => {
     res.send(`
     <h1>Hello King, this is a quotes app.</h1>
@@ -20,12 +22,43 @@ app.get('/', (req, res) => {
     `)
 })
 
+//Quotes
+
 app.get('/quotes', (req, res) => {
     let quotesToSend = quotes.map(quote => {
         let author = authors.find(author => author.id === quote.authorId)
         return{...quote, author}
     })
     res.send(quotesToSend)
+})
+
+app.post('/quotes', (req, res) => {
+    let errors: string[] = []
+
+    if (typeof req.body.description !== 'string'){
+        errors.push('Quote not given')
+    }
+
+    if (typeof req.body.authorId !== 'number'){
+        errors.push('Author not given')
+    }
+
+    let author = authors.find(author => author.id === req.body.authorId)
+    if (!author){
+        errors.push('Author does not exist')
+    }
+
+    if (errors.length === 0){
+        const newQuote = {
+            id: quotes.length === 0 ? 1 : quotes[quotes.length - 1].id + 1, 
+            description: req.body.description,
+            authorId: req.body.authorId
+        }
+        quotes.push(newQuote)
+        res.send(newQuote)
+    } else{
+        res.status(400).send({errors})
+    }
 })
 
 app.get('/quotes/:id', (req, res) => {
@@ -45,12 +78,43 @@ app.get('/randomquotes', (req, res) => {
     res.send(randomItem)
 })
 
+//Authors
+
 app.get('/authors', (req, res) => {
     let authorToSend = authors.map(author => {
         const quotesWithAuthors = quotes.filter(quote => quote.authorId === author.id)
         return {...author, quotes: quotesWithAuthors}
     })
     res.send(authorToSend)
+})
+
+app.post('/authors', (req, res) => {
+    let errors: string[] = []
+
+    if (typeof req.body.name !== 'string'){
+        errors.push('Authors name not given')
+    }
+
+    if (typeof req.body.age !== 'number'){
+        errors.push('Authors age not given')
+    }
+
+    if (typeof req.body.photo !== 'string'){
+        errors.push('Photo of the author is not given')
+    }
+
+    if (errors.length === 0){
+        const newAuthor = {
+            id: authors.length === 0 ? 1 : authors[authors.length - 1].id + 1, 
+            name: req.body.name,
+            age: req.body.age,
+            photo: req.body.photo
+        }
+        authors.push(newAuthor)
+        res.send(newAuthor)
+    } else{
+        res.status(400).send({errors})
+    }
 })
 
 app.get('/authors/:id', (req, res) => {
@@ -69,6 +133,8 @@ app.get('/randomauthors', (req, res) => {
     const randomItem = authors[randomIndex]
     res.send(randomItem)
 })
+
+//General
 
 app.listen(port, () => {
      console.log (`App running on port: ${port} `)
